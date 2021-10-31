ce3b8c5cc6ab75324263f9c073112a8076280c70
ZSR-Blxck <zavierrattray@gmail.com>
2021-10-31 06:03:58 -0500

    Search script still stubborn

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-document.getElementsByClassName("btn").onclick = function(){search()};
+window.onload = $(function() {
+      var name = $('.search').val();
+      if(name == ""){
+        $(".result").html("");
+      }
+      else{
+        $.ajax({
+          type:"POST",
+          url: "superheroes.php",
 
-function search(){
-    alert("location:superheroes.php");
-};
+          data:{
+            search:name
+          },
 
+          success: function(html){
+            $(".result").html(html).show();
+          }
+        });
+      }
+    });
